73a77d6923465a4d5a6faf396a53ad01a8297a68
Chris Wesemael <Chrislbw@hotmail.co.uk>
2017-04-28 23:13:15 +0100

    Added current working version of project. Containing partially working level generation

---
+++ multiLevelDungGame/Assets/Scripts/ItemRandom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRandom : MonoBehaviour {
+
+    public GameObject[] PlaceableItems;
+    public GameObject[] pairObject;
+    private int ItemNum;
+    private int randNumGend;
+
+    void Start () {
+        ItemNum = PlaceableItems.Length;
+
+        randNumGend = Random.Range(0, (ItemNum));
+
+        PlaceableItems[randNumGend].SetActive(true);
+
+        if (pairObject[0] != null)
+        {
+            pairObject[randNumGend].SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
