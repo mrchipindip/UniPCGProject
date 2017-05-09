f8698307781f9eae7257eaa98b04c0b51ec66906
Chris Wesemael <Chrislbw@hotmail.co.uk>
2017-05-09 13:54:06 +0100

    Added Character and all 50 animations to the project/scene. Began making the custom character controller. Animations and movement for forwards backwards and walk in both directions done. Removed all Unity standard assets from the project.

---
+++ multiLevelDungGame/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent] //prevents it from being attached to more than one gameobjcet
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class CharacterMovement : MonoBehaviour {
+    Animator charAnim;
+
+    void Awake()
+    {
+        charAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            charAnim.SetBool("Walk", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            charAnim.SetBool("Walk", false);
+        }
+
+        charAnim.SetFloat("Forward", Input.GetAxisRaw("Vertical"));
+        Debug.Log(Input.GetAxisRaw("Vertical"));
+    }
+
+}
