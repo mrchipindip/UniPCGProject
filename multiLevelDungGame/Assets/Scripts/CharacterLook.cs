bf3151af508362143f8b122c00deadd4e79bd060
Chris Wesemael <Chrislbw@hotmail.co.uk>
2017-05-11 00:23:39 +0100

    Added remaining animations for the main char. progressed the main character controller. Added the majority of the critical animations to the controllers and made the relevant blends.  Added the controller inputs so that they work correctly. Added combo strikes with melee weapons so that they work both visually and in the code. Added full range of movement. still need to work on smoothing the turning correctly

---
+++ multiLevelDungGame/Assets/Scripts/CharacterLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLook : MonoBehaviour {
+    public GameObject mainChar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DoRotation();
+	}
+
+    void DoRotation()
+    {
+
+    }
+}
--- multiLevelDungGame/Assets/Scripts/CharacterMovement.cs
+++ multiLevelDungGame/Assets/Scripts/CharacterMovement.cs
@@ -7,7 +7,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
 public class CharacterMovement : MonoBehaviour {
+
     Animator charAnim;
+    bool jumping = false;
+    bool swinging = false;
+    bool combo1Available = false;
+    bool combo1InUse = false;
+    bool combo2Available = false;
+    bool combo2InUse = false;
 
     void Awake()
     {
@@ -16,7 +23,18 @@ public class CharacterMovement : MonoBehaviour {
 
     void Update()
     {
+        Turn();
         Move();
+        Jump();
+        Swing();
+        ComboOne();
+        ComboTwo();
+    }
+
+    void Turn()
+    {
+        //charAnim.SetFloat("Turning", Input.GetAxis("Horizontal"));
+        //charAnim.SetFloat("Turning", Input.GetAxis("Horizontal2"));
     }
 
     void Move()
@@ -31,8 +49,123 @@ public class CharacterMovement : MonoBehaviour {
             charAnim.SetBool("Walk", false);
         }
 
-        charAnim.SetFloat("Forward", Input.GetAxisRaw("Vertical"));
-        Debug.Log(Input.GetAxisRaw("Vertical"));
+        //charAnim.SetFloat("Forward", Input.GetAxis("Vertical"));
+        charAnim.SetFloat("MoveZ", Input.GetAxis("Vertical2"));
+        charAnim.SetFloat("MoveX", Input.GetAxis("Horizontal2"));
+        //Debug.Log(Input.GetAxisRaw("Vertical") + ";;" + Input.GetAxis("Vertical"));
     }
 
+    void Jump()
+    {
+        Debug.Log(Input.GetAxis("Jump"));
+        if ((Input.GetAxisRaw("Jump") > 0.2) && jumping == false)
+        {
+            jumping = true;
+            charAnim.SetTrigger("Jump");
+            StartCoroutine(JumpTime());
+        }
+    } 
+
+    IEnumerator JumpTime()
+    {
+        yield return new WaitForSeconds(1);
+        jumping = false;
+    }
+
+    void DamageRecieved()
+    {
+        charAnim.SetTrigger("Damaged");
+    }
+
+    void PowerUpCollected()
+    {
+        charAnim.SetTrigger("Powerup");
+    }
+
+    void Swing()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0)) 
+        {
+            if(swinging == false)
+            {
+                if (combo1Available == true)
+                {
+                    charAnim.SetTrigger("Swing2");
+                    swinging = true;
+                    combo1InUse = true;
+                    StartCoroutine(ComboTime());
+                }
+                else if (combo2Available == true)
+                {
+                    Debug.Log("SwingCalled");
+                    charAnim.SetTrigger("Swing3");
+                    swinging = true;
+                    combo2InUse = true;
+                    StartCoroutine(ComboTime());
+                }
+                else
+                {
+                    charAnim.SetTrigger("Swing");
+                    swinging = true;
+                    StartCoroutine(ComboTime());
+                }
+            }
+            Debug.Log("button Pressed");
+            
+        }
+    }
+
+    void ComboOne()
+    {
+        if (combo1Available == true)
+        {
+           
+            StartCoroutine(ComboBoolReset());
+        }
+    }
+
+    void ComboTwo()
+    {
+        if (combo2Available == true)
+        {
+            Debug.Log("ResetCalled");
+            StartCoroutine(ComboBoolReset());
+        }
+    }
+
+
+    IEnumerator ComboTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        swinging = false;
+        if (combo1InUse ==  false && combo2InUse == false)
+        {
+            combo1Available = true;
+        }
+        else if (combo1InUse == true)
+        {
+            combo2Available = true;
+        }
+        combo1InUse = false;
+        combo2InUse = false;
+
+    }
+
+    IEnumerator ComboBoolReset()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if(combo1Available == true)
+        {
+            combo1Available = false;
+        }
+          
+       if(combo2Available == true && combo2InUse == true)
+        {
+            Debug.Log("EarlyReset");
+            combo2Available = false;
+        }  
+
+            
+     
+    }
 }
