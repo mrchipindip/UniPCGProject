e8136e79aeb5b8639c2ba2f432efdcbff52d4b2f
Chris Wesemael <Chrislbw@hotmail.co.uk>
2017-05-13 23:13:58 +0100

    Added full support for blocking into the Character controller. Edited movement so that it forward motions dont conflict when merged. Added second unity folder containing test version of the character controller, using a highly modified version of the standard asset. Looks promising at this time.

---
+++ multiLevelDungGameTEST/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent] //prevents it from being attached to more than one gameobjcet
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class CharacterBehaviour : MonoBehaviour
+{
+
+    Animator charAnim;
+    bool jumping = false;
+    bool swinging = false;
+    bool combo1Available = false;
+    bool combo1InUse = false;
+    bool combo2Available = false;
+    bool combo2InUse = false;
+
+    void Awake()
+    {
+        charAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Turn();
+        //Move();
+        //Jump();
+        Swing();
+        ComboOne();
+        ComboTwo();
+        Blocking();
+    }
+
+    void Turn()
+    {
+        //charAnim.SetFloat("Turning", Input.GetAxis("Horizontal"));
+        //charAnim.SetFloat("Turning", Input.GetAxis("Horizontal2"));
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            charAnim.SetBool("Walk", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            charAnim.SetBool("Walk", false);
+        }
+
+        //charAnim.SetFloat("Forward", Input.GetAxis("Vertical"));
+        charAnim.SetFloat("MoveZ", Input.GetAxis("Vertical2"));
+        charAnim.SetFloat("MoveX", Input.GetAxis("Horizontal2"));
+        //Debug.Log(Input.GetAxisRaw("Vertical") + ";;" + Input.GetAxis("Vertical"));
+    }
+
+    void Jump()
+    {
+        if ((Input.GetAxisRaw("Jump") > 0.2) && jumping == false)
+        {
+            jumping = true;
+            charAnim.SetTrigger("Jump");
+            StartCoroutine(JumpTime());
+        }
+    }
+
+    IEnumerator JumpTime()
+    {
+        yield return new WaitForSeconds(1);
+        jumping = false;
+    }
+
+    void DamageRecieved()
+    {
+        charAnim.SetTrigger("Damaged");
+    }
+
+    void PowerUpCollected()
+    {
+        charAnim.SetTrigger("Powerup");
+    }
+
+    void Swing()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            if (swinging == false)
+            {
+                if (combo1Available == true)
+                {
+                    charAnim.SetTrigger("Swing2");
+                    swinging = true;
+                    combo1InUse = true;
+                    StartCoroutine(ComboTime());
+                }
+                else if (combo2Available == true)
+                {
+                    charAnim.SetTrigger("Swing3");
+                    swinging = true;
+                    combo2InUse = true;
+                    StartCoroutine(ComboTime());
+                }
+                else
+                {
+                    charAnim.SetTrigger("Swing");
+                    swinging = true;
+                    StartCoroutine(ComboTime());
+                }
+            }
+
+        }
+    }
+
+    void ComboOne()
+    {
+        if (combo1Available == true)
+        {
+
+            StartCoroutine(ComboBoolReset());
+        }
+    }
+
+    void ComboTwo()
+    {
+        if (combo2Available == true)
+        {
+            StartCoroutine(ComboBoolReset());
+        }
+    }
+
+
+    IEnumerator ComboTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        swinging = false;
+        if (combo1InUse == false && combo2InUse == false)
+        {
+            combo1Available = true;
+        }
+        else if (combo1InUse == true)
+        {
+            combo2Available = true;
+        }
+        combo1InUse = false;
+        combo2InUse = false;
+
+    }
+
+    IEnumerator ComboBoolReset()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (combo1Available == true)
+        {
+            combo1Available = false;
+        }
+
+        if (combo2Available == true && combo2InUse == true)
+        {
+            combo2Available = false;
+        }
+
+
+
+    }
+
+    void Blocking()
+    {
+        if (Input.GetButtonDown("Block"))
+        {
+            charAnim.SetBool("Blocking", true);
+        }
+        if (Input.GetButtonUp("Block"))
+        {
+            charAnim.SetBool("Blocking", false);
+        }
+    }
+}
---
+++ multiLevelDungGameTEST/Assets/Scripts/ModularWorldGenerator.cs
@@ -0,0 +1,176 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ModularWorldGenerator : MonoBehaviour
+{
+	public Module[] Modules;
+    public Module exitCloser;
+    public Module endRoom;
+	public Module StartModule;
+
+	public int Iterations = 5;
+
+    private bool endRoomPresent = false;
+
+    void Start()
+    {
+        //Instan a starting mdule
+        var startModule = (Module)Instantiate(StartModule, transform.position, transform.rotation);
+        var pendingExits = new List<ModuleConnector>(startModule.GetExits());
+
+        List<Module> allRooms = new List<Module>();
+        allRooms.Add(startModule);
+
+            for (int iteration = 0; iteration < Iterations; iteration++)
+            {
+                //def of exit list
+                var newExits = new List<ModuleConnector>();
+                //for every exit in the list
+                foreach (var pendingExit in pendingExits)
+                {
+                    bool colPresent = true;
+                    int infPreventer = 0;
+
+                    while (colPresent == true)
+                    {
+                        //conn a valid module and add its exits to the new list
+                        var newTag = GetRandom(pendingExit.Tags);
+                        //Select a random room with the tag of the exit being checked
+                        var newModulePrefab = GetRandomWithTag(Modules, newTag);
+                        //instantiate the randomed module
+                        var newModule = (Module)Instantiate(newModulePrefab);
+                        //Add the exits of the newly created module too a seperate list
+                        var newModuleExits = newModule.GetExits();
+                        //Gte the Exit of the new module that will be matched with the existing exit
+                        var exitToMatch = newModuleExits.FirstOrDefault(x => x.IsDefault) ?? GetRandom(newModuleExits);
+                        //Match these exits
+                        MatchExits(pendingExit, exitToMatch);
+
+                        if (CheckCollisions(newModule, allRooms) == true)
+                        {
+                            Destroy(newModule.gameObject);
+                            Debug.Log("Bingo");
+                        }
+                        else if (CheckCollisions(newModule, allRooms) == false)
+                        {
+                            newExits.AddRange(newModuleExits.Where(e => e != exitToMatch));
+                            allRooms.Add(newModule);
+                            colPresent = false;
+                        }
+
+                        if (infPreventer > 35)
+                        {
+                            colPresent = false;
+                        }
+                        infPreventer++;
+
+                        //Add all of the exits from the new module to the existing list Excluding the one that was joined
+
+                    }
+
+                }
+                //add all unconnected exits to the pending exits list
+                Debug.Log("num: " + iteration);
+                pendingExits = newExits;
+            }
+
+            //Close exits and place final room
+            foreach (var pendingExit in pendingExits)
+            {
+
+                if (endRoomPresent == false)
+                {
+                    //place final room and match the exit
+                    var newModule = (Module)Instantiate(endRoom);
+                    var newModuleExits = newModule.GetExits();
+                    var exitToMatch = newModuleExits.FirstOrDefault(x => x.IsDefault) ?? GetRandom(newModuleExits);
+                    MatchExits(pendingExit, exitToMatch);
+                    //If placed end room collides with another - Destroy - close exit
+                    if (CheckCollisions(newModule, allRooms) == true)
+                    {
+                        //destroy end room
+                        Destroy(newModule.gameObject);
+                        //close exit
+                        var newCloseModule = (Module)Instantiate(endRoom);
+                        var newCloseModuleExits = newCloseModule.GetExits();
+                        var exitToMatchClose = newCloseModuleExits.FirstOrDefault(x => x.IsDefault) ?? GetRandom(newModuleExits);
+                        MatchExits(pendingExit, exitToMatchClose);
+                    }
+                    else if (CheckCollisions(newModule, allRooms) == false)
+                    {
+                        endRoomPresent = true;
+                    }
+
+
+                }
+                //Final Room already present - close all remaining exits
+                else
+                {
+                    //instantiate the closing module
+                    var newModule = (Module)Instantiate(exitCloser);
+                    //Get room closers exits
+                    var newModuleExits = newModule.GetExits();
+
+                    var exitToMatch = newModuleExits.FirstOrDefault(x => x.IsDefault) ?? GetRandom(newModuleExits);
+                    //Match these exits
+                    MatchExits(pendingExit, exitToMatch);
+                }
+
+            }
+
+    }
+
+
+	private void MatchExits(ModuleConnector oldExit, ModuleConnector newExit)
+	{
+		var newModule = newExit.transform.parent;
+		var forwardVectorToMatch = -oldExit.transform.forward;
+        //corr z & y axis rot
+		var correctiveRotation = Azimuth(forwardVectorToMatch) - Azimuth(newExit.transform.forward);
+		newModule.RotateAround(newExit.transform.position, Vector3.up, correctiveRotation);
+        //get the trans required for exit to meet
+		var correctiveTranslation = oldExit.transform.position - newExit.transform.position;
+        //move it so they meet
+		newModule.transform.position += correctiveTranslation;
+	}
+
+
+	private static TItem GetRandom<TItem>(TItem[] array)
+	{
+		return array[Random.Range(0, array.Length)];
+	}
+
+
+	private static Module GetRandomWithTag(IEnumerable<Module> modules, string tagToMatch)
+	{
+		var matchingModules = modules.Where(m => m.Tags.Contains(tagToMatch)).ToArray();
+		return GetRandom(matchingModules);
+	}
+
+
+	private static float Azimuth(Vector3 vector)
+	{
+		return Vector3.Angle(Vector3.forward, vector) * Mathf.Sign(vector.x);
+	}
+
+    //checks coll between rooms
+    private bool CheckCollisions(Module moduleToCheck, List<Module> activeModules)
+    {
+        Collider newCollider = moduleToCheck.transform.GetComponent<Collider>();
+        Collider compareCollider = new Collider();
+
+        for (int i = 0; i < activeModules.Count; i++)
+        {
+            compareCollider = activeModules[i].transform.GetComponent<Collider>();
+            
+            if(newCollider.bounds.Intersects(compareCollider.bounds))
+            {
+                Debug.Log("Coll Detected");
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
